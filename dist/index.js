0ada690c220c6304c71a8c358d61a279c309c6cb
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-06-19 09:30:46 +0300

    More bugfixes

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,33 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.verifyToken = verifyToken;
+exports.verifyToken = undefined;
+
+var verifyToken = exports.verifyToken = function () {
+    var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
+        var _args = arguments;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+            while (1) {
+                switch (_context.prev = _context.next) {
+                    case 0:
+                        _context.next = 2;
+                        return _verifyToken2.default.apply(undefined, _args);
+
+                    case 2:
+                        return _context.abrupt('return', _context.sent);
+
+                    case 3:
+                    case 'end':
+                        return _context.stop();
+                }
+            }
+        }, _callee, this);
+    }));
+
+    return function verifyToken() {
+        return ref.apply(this, arguments);
+    };
+}();
 
 var _verifyToken = require('./authentication/verifyToken');
 
@@ -11,8 +37,4 @@ var _verifyToken2 = _interopRequireDefault(_verifyToken);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
-function verifyToken(params) {
-    _verifyToken2.default.apply(undefined, _toConsumableArray(params));
-}
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { return step("next", value); }, function (err) { return step("throw", err); }); } } return step("next"); }); }; }
