e80c47709d5974c233cc2b5fdbfac5cbd1f0136f
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-06-19 09:14:31 +0300

    Fix build

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,8 @@ var _verifyToken2 = _interopRequireDefault(_verifyToken);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function verifyToken() {
-    (0, _verifyToken2.default)();
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function verifyToken(params) {
+    _verifyToken2.default.apply(undefined, _toConsumableArray(params));
 }
